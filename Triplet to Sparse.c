09a4a0cce6b8a6257cc082c41bc0dfbd84150d6f
Jitesh Bhakat <137819786+Jitesh8260@users.noreply.github.com>
2023-11-28 23:05:55 +0530

    Create Triplet to Sparse

---
+++ Triplet to Sparse.c
@@ -0,0 +1,59 @@
+//program to form sparse matrix from triplet form
+#include<stdio.h>
+void readmatrix(int[][3],int );
+void createsparse(int [][3], int ,int ,int[][50] );
+void displaysparse(int[][50]);
+int main()
+{
+	int sparse[50][50],triplet[50][3],r,c,i,j,nz;
+	printf("enter the no of rows triplet matrix:\n");
+	scanf("%d",&r);
+	printf("enter the number of non zero terms\n");
+	scanf("%d",&nz);
+	readmatrix(triplet,r);
+	printf("\n the triplet matrix is \n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",triplet[i][j]);
+
+		}
+		printf("\n");
+	}
+	createsparse(sparse,r,c,triplet);
+	displaysparse(triplet);
+	return 0;
+}
+void readmatrix(int triplet[][3],int r)
+{
+	int i,j;
+	printf("enter the elements of the matrix:\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&triplet[i][j]);
+		}
+	}
+}
+void createsparse(int triplet[][3],int r,int nz,int sparse[][50] )
+{
+	int i,j;
+	for(i=0;i<nz;i++)
+	{
+		sparse[triplet[i][0]][triplet[i][1]]=triplet[i][2];
+	}
+}
+			
+void displaysparse(int sparse[][50])
+{
+	int i,j;
+	printf("\nthe sparse matrix is: \n");
+	for(i=0;i<=nz;i++)
+	{
+		for(j=0;j<3;j++)
+			printf("%d\t",sparse[i][j]);
+		printf("\n");
+	}
+}

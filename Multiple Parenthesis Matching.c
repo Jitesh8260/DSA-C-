d0bf3bc91bc837c589a051658392a149397bb496
Jitesh Bhakat <137819786+Jitesh8260@users.noreply.github.com>
2023-12-12 21:52:45 +0530

    Add files via upload

---
+++ Multiple Parenthesis Matching.c
@@ -0,0 +1,80 @@
+//program for multiple parenthesis matching
+#include<stdio.h>
+#define max 100
+char stack[max];
+int top=-1;
+void push(char[], char);
+int pop();
+int main()
+{
+	int i;
+	char inf[max];
+	printf("enter the expression: \n");
+	scanf("%s",inf);
+	for(i=0;inf[i]!='\0';i++)
+	{
+		if(inf[i]=='(' || inf[i]=='[' || inf[i]=='{')
+		{
+			push(stack,inf[i]);
+		}
+		else if(inf[i]==')' || inf[i]==']' || inf[i]=='}')
+		{
+			if(inf[i]==')')
+			{
+				if(stack[top]=='(')
+					pop();
+				else
+					printf("not balanced\n");
+				break;
+			}
+			else if(inf[i]==']')
+			{
+				if(stack[top]=='[')
+					pop();
+				else
+					printf("not balanced\n");
+				break;
+			}
+			else if(inf[i]=='}')
+			{
+				if(stack[top]=='{')
+					pop();
+				else
+					printf("not balanced\n");
+				break;
+			}
+		}
+	}
+	if(top==-1)
+		printf("the expression is BALANCED\n");
+	else
+		printf("the expression is NOT BALANCED\n");
+}
+void push(char stack[max],char item)
+{
+	if(top==max-1)
+	{
+		printf("stack overflow\n");
+	}
+	else
+		top++;
+	stack[top]=item;
+}
+int pop()
+{
+	char item;
+	if(top==-1)
+	{
+		printf("\nstack underflow");
+		return;
+	}
+	item=stack[top];
+	top--;
+	return item;
+}
+
+				
+			
+			
+
+
